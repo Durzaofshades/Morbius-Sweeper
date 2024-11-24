05991fd02c74527c3e510ed8d2575fec1b2cdf34
Jacob Boots <jacob.boots@wsu.edu>
2024-11-23 18:14:47 -0800

    Add project files.

---
+++ Tile.hpp
@@ -0,0 +1,8 @@
+// Class for each on screen tile, which are either a mine or a clue
+
+class Tile {
+public:
+	int clue; // -1 for mine, otherwise is the number of mines in the surrounding 8 tiles
+
+
+};
