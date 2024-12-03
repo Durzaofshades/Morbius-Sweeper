5ba5f3b1af4353a597fbd050862e66c42cf13964
Samuel Toompas <sam@toompas.com>
2024-12-02 19:30:00 -0800

    Cleaned Up

--- src/appWrapper.cpp
+++ src/appWrapper.cpp
@@ -1,22 +1,5 @@
 #include "appWrapper.hpp"
 
-
-//void make_box(sf::RenderWindow& window) {
-//	float start_x = X_OFFSET, 
-//	float start_y = Y_OFFSET;
-//
-//	for (int i = 0; i < 15; i++) {
-//		for (int j = 0; j < 15; j++) {
-//			Tile* name = (Tile*) new Tile(start_x, start_y);
-//			window.draw(name->m_hidden_Tile);
-//			start_x = start_x + TILE_LENGTH;
-//		}
-//
-//		start_x = X_OFFSET;
-//		start_y = Y_OFFSET + start_y;
-//	}
-//}
-
 void app_wrapper() {
 	// Minefield_Width;
 	// Minefield_Height;
