5c9de1057a5b77fc8ea6671dd5a0c1801c05c424
Jacob Boots <jacob.boots@wsu.edu>
2024-12-05 17:33:42 -0800

    maybe

--- src/appWrapper.cpp
+++ src/appWrapper.cpp
@@ -6,14 +6,17 @@ void app_wrapper() {
 
 	// open window
 	sf::RenderWindow window(sf::VideoMode(Window_Width, Window_Height), "Morbius Sweeper");
-
+	sf::Vector2i position = sf::Mouse::getPosition();
+	sf::Mouse::setPosition(sf::Vector2i(Window_Width, Window_Height), window);
 	while (window.isOpen()) {
 		sf::Event event;
 
 		while (window.pollEvent(event)) { // check for event
 			if (event.type == sf::Event::Closed) window.close();
-			if (event.type == sf::Mouse::Left) minefield.reveal(sf::Mouse::getPosition(window).x, sf::Mouse::getPosition(window).y);
-			if (event.type == sf::Mouse::Right) minefield.flag(sf::Mouse::getPosition(window).x, sf::Mouse::getPosition(window).y);
+			if (sf::Mouse::isButtonPressed(sf::Mouse::Left)) {
+				minefield.reveal(sf::Mouse::getPosition(window).x, sf::Mouse::getPosition(window).y);
+			}
+			if (sf::Mouse::isButtonPressed(sf::Mouse::Right))minefield.flag(sf::Mouse::getPosition(window).x, sf::Mouse::getPosition(window).y);
 		}
 
 		// update window
