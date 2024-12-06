2150e3b87b8d47081c6e61d4251547db77688d4b
Samuel Toompas <sam@toompas.com>
2024-12-05 17:39:59 -0800

    ///

--- src/appWrapper.cpp
+++ src/appWrapper.cpp
@@ -12,8 +12,10 @@ void app_wrapper() {
 
 		while (window.pollEvent(event)) { // check for event
 			if (event.type == sf::Event::Closed) window.close();
-			if (event.type == sf::Mouse::Left) minefield.reveal(sf::Mouse::getPosition(window).x, sf::Mouse::getPosition(window).y);
-			if (event.type == sf::Mouse::Right) minefield.flag(sf::Mouse::getPosition(window).x, sf::Mouse::getPosition(window).y);
+			//if (event.type == sf::Mouse::Left) minefield.reveal(sf::Mouse::getPosition(window).x, sf::Mouse::getPosition(window).y);
+			//if (event.type == sf::Mouse::Right) minefield.flag(sf::Mouse::getPosition(window).x, sf::Mouse::getPosition(window).y);
+			if (sf::Mouse::isButtonPressed(sf::Mouse::Left))minefield.reveal(sf::Mouse::getPosition(window).x, sf::Mouse::getPosition(window).y);
+			if (sf::Mouse::isButtonPressed(sf::Mouse::Right))minefield.flag(sf::Mouse::getPosition(window).x, sf::Mouse::getPosition(window).y);
 		}
 
 		// update window
