9010db851cb3e7283fd8cc81293f31755ccb68c5
Samuel Toompas <sam@toompas.com>
2024-12-05 17:27:44 -0800

    fixing

--- src/Minefield.cpp
+++ src/Minefield.cpp
@@ -113,8 +113,8 @@ void Minefield::flag(int x, int y) {
 	int X_index = (X_pos - X_OFFSET) / TILE_LENGTH;
 	int Y_index = (Y_pos - Y_OFFSET) / TILE_LENGTH;
 	
-	if (0 <= X_index >= Minefield_Width) return;
-	if (0 <= Y_index >= Minefield_Height) return;
+	if (0 <= X_index < Minefield_Width) return;
+	if (0 <= Y_index < Minefield_Height) return;
 
 	Tile* current_Tile = this->field[X_index][Y_index];
 
