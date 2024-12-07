9fe585cc6f4ff94d9ad9789301d40416c2288e66
Samuel Toompas <sam@toompas.com>
2024-12-06 20:52:45 -0800

    more fixes

--- src/Textures.hpp
+++ src/Textures.hpp
@@ -4,14 +4,14 @@ class Textures {
 public:
 	Textures(void);
 
-	sf::Texture tile_hidden;
-	sf::Texture tile_flag;
-	sf::Texture tile_m1;
-	sf::Texture tile_m2;
-	sf::Texture tile_m3;
-	sf::Texture tile_m4;
-	sf::Texture tile_m5;
-	sf::Texture tile_m6;
-	sf::Texture tile_m7;
-	sf::Texture tile_m8;
+	sf::Texture tile_hidden; // hidden textures
+	sf::Texture tile_flag; // flag texture
+	sf::Texture tile_m1; // clue 1
+	sf::Texture tile_m2; // clue 2
+	sf::Texture tile_m3; // clue 3
+	sf::Texture tile_m4; // clue 4
+	sf::Texture tile_m5; // clue 5
+	sf::Texture tile_m6; // clue 6
+	sf::Texture tile_m7; // clue 7
+	sf::Texture tile_m8; // clue 8
 };
