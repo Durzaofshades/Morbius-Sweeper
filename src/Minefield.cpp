066524c5c742bfd69f92e56d5af2cec2fb20448f
Samuel Toompas <sam@toompas.com>
2024-12-06 19:41:42 -0800

    Finished generate field

--- src/Minefield.cpp
+++ src/Minefield.cpp
@@ -51,21 +51,14 @@ void Minefield::generate_field() {
 			this->field[x_index][y_index] = new Mine_Tile(x_pos, y_pos);
 		}
 	}
-	/*
-	for (int x_index = 0; x_index < Minefield_Width; x_index++) {
-		for (int y_index = 0; y_index < Minefield_Height; y_index++) {
-			// loop over every tile
-			// if tile is not a mine, create a new clue tile
-			x_pos = X_OFFSET + (x_index * TILE_LENGTH);
-			y_pos = Y_OFFSET + (y_index * TILE_LENGTH);
 
-			if (!this->field[x_index][y_index]->is_Mine()) {
-				// if the current mine index is NOT a mine:
-				this->field[x_index][y_index] = (Tile*)(new Clue_Tile(this->field, x_pos, y_pos, tile_hidden));
+	for (x = 0; x < Minefield_Width; x++) {
+		for (y = 0; y < Minefield_Width; y++) {
+			if (field[x][y]->is_mine == 0){
+				((Clue_Tile*)field[x][y])->calculate_clue(field, x, y);
 			}
 		}
 	}
-	*/
 }
 
 void Minefield::lose_game()
